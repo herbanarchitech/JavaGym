8f31964acfa426d454a03ba426c9fda3bf952f64
herbanarhitech <jeff.badette@protonmail.com>
2021-09-30 20:59:42 -0400

    included me

---
+++ ModThreeWeek1/src/VariablesAndCondition/varIablesAndPackages.java
@@ -0,0 +1,32 @@
+package VariablesAndCondition;
+/* 
+    Write an if/else statement for the following requirements:
+        If student gets 90 or higher: console log  A
+        If students get 80 or above: console log B
+        If students get 70 or above: console log C
+        If students get 55 or above: console log D
+        Any grade lower than 55 is F
+    Using switch case, do the following:
+        Store a number between 1-7
+        If the number is 1, display a message "Monday", 2 for "Tuesday", 3 for "Wednesday" etc.
+        Anything other than 1-7 would default to "Invalid Input"
+    Store a number in a variable called num and write an if statement: 
+        If num is odd, display "Cool"
+        If num is even and between 2-5, display "Not Cool"
+        If num is even and between 6-20, display "Cool"
+        If num is even and greater than 20, display "Not Cool
+*/
+public class varIablesAndPackages {
+	public static void main(String[] args) {
+//writing 
+
+int i;
+for(i=0;i<=50; i++) {
+
+	System.out.print(i);
+	
+	
+}
+	}
+}
+
---
+++ ModThreeWeek1/src/module-info.java
@@ -0,0 +1,2 @@
+module ModThreeWeek1 {
+}
